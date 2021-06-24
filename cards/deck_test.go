4a67d39a62085117c7c5c5df62dd602c270e2997
JS <r0blox1053@outlook.com>
2021-06-24 16:04:02 -0700

    Finish

--- cards/deck_test.go
+++ cards/deck_test.go
@@ -1,2 +1,35 @@
 package main
 
+import (
+	"testing"
+	"os"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 44 {
+		t.Errorf("Expected deck length of 16, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades" {
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "King of Clubs" {
+		t.Errorf("Expected last card of King of Clubs, but got %v", d[len(d) - 1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckTestFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 44 {
+		t.Errorf("Expected 44 cards, got %v", len(loadedDeck))
+	}
+}
---
+++ cards/go.mod
@@ -0,0 +1,3 @@
+module github.com/oonqt/go-tutorial
+
+go 1.16
