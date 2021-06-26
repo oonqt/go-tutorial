79fe3020cb6eccbfb1e7d7754f243f7d083270bf
JS <r0blox1053@outlook.com>
2021-06-26 15:15:21 -0700

    BYE-

--- advanced/channels/main.go
+++ advanced/channels/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,20 +16,38 @@ func main() {
 		"https://catalog.roblox.com",
 		"https://groups.roblox.com",
 		"https://trades.roblox.com",
+		"https://privatemessages.roblox.com",
 		"https://api.roblox.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
+	}
+
+	// for i := 0; i < len(links); i++ {
+	// 	go checkLink(<-c, c)
+	// }
+
+	// for {
+	// 	go checkLink(<-c, c)
+	// }
+
+	for l := range c {
+		time.Sleep(5 * time.Second)
+		go checkLink(l, c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "Is down")
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "Is up")
+	c <- link
 }
